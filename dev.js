ad198059a4a1e78233c15ca7e1d288910470a4eb
Alger Makiputin <hello@algermakiputin.dev>
2025-05-16 13:14:12 +0800

    update index file

--- dev.js
+++ dev.js
@@ -1,29 +1,18 @@
-import { ApolloServer, gql } from "apollo-server-express";
-import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
-import http from "http";
-import express from "express";
-import cors from "cors";
+// vercel app
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
 import { typeDefs } from './schema.js';
 import { resolvers } from './resolvers.js';
 import { context } from './app/context/context.js';
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-const httpServer = http.createServer(app);
-
-const startApolloServer = async(app, httpServer) => {
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context,
-    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
-  });
-
-  await server.start();
-  server.applyMiddleware({ app });
-}
-
-startApolloServer(app, httpServer);
-
-export default httpServer;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+  
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 3333 },
+  context,
+});
+  
+console.log(`🚀  Server ready at: ${url}`);
---
+++ index.js
@@ -0,0 +1,29 @@
+import { ApolloServer, gql } from "apollo-server-express";
+import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
+import http from "http";
+import express from "express";
+import cors from "cors";
+import { typeDefs } from './schema.js';
+import { resolvers } from './resolvers.js';
+import { context } from './app/context/context.js';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+const httpServer = http.createServer(app);
+
+const startApolloServer = async(app, httpServer) => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+
+  await server.start();
+  server.applyMiddleware({ app });
+}
+
+startApolloServer(app, httpServer);
+
+export default httpServer;
